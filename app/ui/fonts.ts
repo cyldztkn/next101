171b8116a8bdd3baee6c6b673b87798c02800bf5
Cagkan <99582229+cyldztkn@users.noreply.github.com>
2024-04-17 02:50:50 +0300

    build

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
